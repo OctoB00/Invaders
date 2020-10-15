3fc990e3709eb6ed322568803bc0c55747303f88
OliverMal <70704225+OliverMal@users.noreply.github.com>
2020-10-15 15:15:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
-﻿using System;
+using System;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 
 namespace Invaders
 {
-    class Program
+    public class Program
     {
         public const int ScreenW = 400;
         public const int ScreenH = 800;
         static void Main(string[] args)
         {
+            bool gamestart = true;
             using (var window = new RenderWindow(new VideoMode(ScreenW, ScreenH), "Invaders"))
             {
                 window.Closed += (s, e) => window.Close();
                 Clock frameclock = new Clock();
                 Playership player = new Playership()
                 {
-                    Velocity = new Vector2f(0, 0),
+                    //Velocity = new Vector2f(0, 0),
                     Position = new Vector2f(ScreenW / 2, ScreenH / 2)
                 };
                 Game game = new Game(window);
@@ -27,7 +28,11 @@ namespace Invaders
                     window.Clear(new Color(0, 0, 0));
                     float deltaTime = frameclock.Restart().AsSeconds();
                     game.RenderAll(window);
-                    game.Spawn(player);
+                    if (gamestart)
+                    {
+                        game.Spawn(player);
+                        gamestart = false;
+                    }
                     game.UpdateAll(deltaTime);
 
                     window.Display();
